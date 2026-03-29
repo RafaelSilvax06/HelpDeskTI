33e6c3ffe0fdebf063d8f70b8df3a1514058796e
RafaelSilvax06 <rafael.silva.6806@gmail.com>
2026-03-29 19:45:36 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDeskTI.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        [Required]
+        public string Perfil { get; set; } // Admin, Tecnico, Usuario
+
+        public bool Ativo { get; set; } = true;
+    }
+}
