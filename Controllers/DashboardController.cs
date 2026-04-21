902dc7965cdd9576396952e13ae6b3d63075ed75
Rafael <rafael.silva.6806@gmail.com>
2026-04-21 18:27:00 -0300

    feat: Login funcional + redirecionamento de tela se for Analista ou Cliente.

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class DashboardController : Controller
+{
+    public IActionResult Analista()
+    {
+        return View();
+    }
+
+    public IActionResult Cliente()
+    {
+        return View();
+    }
+}
